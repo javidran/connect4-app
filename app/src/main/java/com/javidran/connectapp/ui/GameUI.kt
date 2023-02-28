303a8d4252ced499901547aaa237255ec56784f0
Javier Cabrera <javidran80@gmail.com>
2023-03-01 00:40:36 +0100

    Tablero de fondo

--- app/src/main/java/com/javidran/connectapp/ui/GameUI.kt
+++ app/src/main/java/com/javidran/connectapp/ui/GameUI.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,17 +16,14 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.javidran.connectapp.R
-import com.javidran.connectapp.datasource.Game
 import com.javidran.connectapp.enums.Disk
 import com.javidran.connectapp.enums.Player
 import com.javidran.connectapp.movelview.GameViewModel
 import com.javidran.connectapp.ui.theme.ConnectAppTheme
-import com.javidran.connectapp.ui.theme.LineColor
 import com.javidran.connectapp.ui.theme.RedPlayerColor
 import com.javidran.connectapp.ui.theme.YellowPlayerColor
 
@@ -36,32 +32,36 @@ fun ActivityScreen(gameViewModel: GameViewModel) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         WinCount(gameViewModel)
         Spacer(modifier = Modifier.weight(1.0f))
-        GameZone(gameViewModel)
+        GameZone(gameViewModel, modifier = Modifier.weight(2.0f) )
         Spacer(modifier = Modifier.weight(1.0f))
         ResetButtons(gameViewModel)
     }
 }
 
 @Composable
-private fun GameZone(gameViewModel: GameViewModel) {
-    if (gameViewModel.winner.value != Player.None || gameViewModel.tie.value) {
-        if (gameViewModel.tie.value) {
-            Text(stringResource(R.string.tie), fontSize = 40.sp)
+private fun GameZone(gameViewModel: GameViewModel, modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier.fillMaxWidth()) {
+        if (gameViewModel.winner.value != Player.None || gameViewModel.tie.value) {
+            if (gameViewModel.tie.value) {
+                Text(stringResource(R.string.tie), fontSize = 40.sp)
+            } else {
+                WinnerText(gameViewModel.winner.value)
+            }
+            Board(
+                grid = gameViewModel.grid.value,
+                onColumnPressed = { },
+                winners = gameViewModel.winningCombination.value,
+                modifier = Modifier.fillMaxWidth()
+            )
         } else {
-            WinnerText(gameViewModel.winner.value)
+            Turn(player = gameViewModel.turn.value)
+            Board(
+                grid = gameViewModel.grid.value,
+                onColumnPressed = gameViewModel::addDisk,
+                winners = emptyList(),
+                modifier = Modifier.fillMaxWidth()
+            )
         }
-        DiskGrid(
-            grid = gameViewModel.grid.value,
-            onColumnPressed = { },
-            winners = gameViewModel.winningCombination.value
-        )
-    } else {
-        Turn(player = gameViewModel.turn.value)
-        DiskGrid(
-            grid = gameViewModel.grid.value,
-            onColumnPressed = gameViewModel::addDisk,
-            winners = emptyList()
-        )
     }
 }
 
@@ -147,72 +147,57 @@ private fun translatePlayerName(player: Player, context: Context): String {
 }
 
 @Composable
-fun DiskGrid(
+fun Board(
     grid: Array<Array<Disk>>,
     onColumnPressed: (column: Int) -> Unit,
-    winners: List<Pair<Int, Int>>
+    winners: List<Pair<Int, Int>>,
+    modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = Modifier.width(IntrinsicSize.Min),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Divider(color = LineColor, thickness = Dp.Hairline)
-        Row(modifier = Modifier.height(IntrinsicSize.Min)) {
-            Spacer(modifier = Modifier.weight(1.0f))
+    Box(
+        modifier = modifier.padding(10.dp)
+    ){
+        Row(Modifier.height(IntrinsicSize.Min)) {
             grid.forEachIndexed { index, arrayOfDisks ->
-                if (index == 0) {
-                    Divider(
-                        color = LineColor,
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .width(1.dp)
-                    )
-                }
-                DiskColumn(
+                BoardColumn(
                     diskArray = arrayOfDisks,
                     columnNumber = index,
                     onColumnPressed = onColumnPressed,
-                    winners = winners.filter { pair -> pair.first == index }
-                        .map { pair -> pair.second }
-                )
-                Divider(
-                    color = LineColor,
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .width(1.dp)
+                    winners = winners
+                        .filter { pair -> pair.first == index }
+                        .map { pair -> pair.second },
+                    modifier = Modifier.wrapContentHeight()
                 )
             }
-            Spacer(modifier = Modifier.weight(1.0f))
         }
-        Divider(color = LineColor, thickness = Dp.Hairline)
+        BoardImage(Modifier.fillMaxWidth().wrapContentHeight())
     }
+
 }
 
 @Composable
-fun DiskColumn(
+fun BoardColumn(
     diskArray: Array<Disk>,
     columnNumber: Int,
     onColumnPressed: (column: Int) -> Unit,
-    winners: List<Int>
+    winners: List<Int>,
+    modifier: Modifier = Modifier
 ) {
     Column(
-        Modifier
-            .width(IntrinsicSize.Min)
-            .clickable { onColumnPressed(columnNumber) }
+        modifier.wrapContentHeight().clickable { onColumnPressed(columnNumber) }
     ) {
         for (i in diskArray.indices.reversed()) {
-            if (i == 0) {
-                Divider(color = LineColor, thickness = Dp.Hairline)
-            }
-            Disk(diskArray[i], winners.contains(i))
-            Divider(color = LineColor, thickness = Dp.Hairline)
+            Disk(
+                disk = diskArray[i],
+                winner = winners.contains(i),
+                modifier = Modifier.fillMaxWidth().weight(1.0f)
+            )
         }
     }
 }
 
 @Composable
 fun Disk(disk: Disk, winner: Boolean, modifier: Modifier = Modifier) {
-    Box(modifier = modifier.padding(2.dp)) {
+    Box(modifier = modifier) {
         when (disk) {
             Disk.Red -> {
                 if (winner) {
@@ -240,7 +225,16 @@ fun DiskImage(@DrawableRes id: Int, modifier: Modifier = Modifier) {
     Image(
         painter = painterResource(id = id),
         contentDescription = null,
-        modifier = modifier.size(50.dp)
+        modifier = modifier
+    )
+}
+
+@Composable
+fun BoardImage(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.board),
+        contentDescription = null,
+        modifier = modifier
     )
 }
 
@@ -249,33 +243,33 @@ fun DiskImage(@DrawableRes id: Int, modifier: Modifier = Modifier) {
 fun DiskImagePreview() {
     ConnectAppTheme {
         Column {
-            DiskImage(id = R.drawable.red_disk)
-            DiskImage(id = R.drawable.winner_red_disk)
-            DiskImage(id = R.drawable.yellow_disk)
-            DiskImage(id = R.drawable.winner_yellow_disk)
-            DiskImage(id = R.drawable.empty_disk)
+            DiskImage(id = R.drawable.red_disk, modifier = Modifier.size(50.dp))
+            DiskImage(id = R.drawable.winner_red_disk, modifier = Modifier.size(50.dp))
+            DiskImage(id = R.drawable.yellow_disk, modifier = Modifier.size(50.dp))
+            DiskImage(id = R.drawable.winner_yellow_disk, modifier = Modifier.size(50.dp))
+            DiskImage(id = R.drawable.empty_disk, modifier = Modifier.size(50.dp))
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun RedDisk() {
+fun RedDiskPreview() {
     ConnectAppTheme {
         Row {
-            Disk(Disk.Red, false)
-            Disk(Disk.Red, true)
+            Disk(Disk.Red, false, modifier = Modifier.size(50.dp))
+            Disk(Disk.Red, true, modifier = Modifier.size(50.dp))
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun YellowDisk() {
+fun YellowDiskPreview() {
     ConnectAppTheme {
         Row {
-            Disk(Disk.Yellow, false)
-            Disk(Disk.Yellow, true)
+            Disk(Disk.Yellow, false, modifier = Modifier.size(50.dp))
+            Disk(Disk.Yellow, true, modifier = Modifier.size(50.dp))
         }
     }
 }
@@ -283,25 +277,36 @@ fun YellowDisk() {
 @Preview(showBackground = true)
 @Composable
 fun DiskColumnPreview() {
-    val game = Game()
-
     val array = arrayOf(Disk.Red, Disk.Red, Disk.Yellow, Disk.Red, Disk.Empty, Disk.Empty)
     ConnectAppTheme {
-        DiskColumn(diskArray = array, columnNumber = 0, game::addDisk, emptyList())
+        BoardColumn(
+            diskArray = array,
+            columnNumber = 0,
+            onColumnPressed = { },
+            winners = emptyList(),
+            modifier = Modifier.width(50.dp).wrapContentHeight()
+        )
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GridPreview() {
-    val game = Game()
-
     val array1 = arrayOf(Disk.Red, Disk.Red, Disk.Yellow, Disk.Red, Disk.Empty, Disk.Empty)
     val array2 = arrayOf(Disk.Red, Disk.Red, Disk.Empty, Disk.Empty, Disk.Empty, Disk.Empty)
     val array3 = arrayOf(Disk.Red, Disk.Empty, Disk.Empty, Disk.Empty, Disk.Empty, Disk.Empty)
-    val grid = arrayOf(array1, array2, array3)
+    val array4 = arrayOf(Disk.Red, Disk.Empty, Disk.Empty, Disk.Empty, Disk.Empty, Disk.Empty)
+    val array5 = arrayOf(Disk.Red, Disk.Empty, Disk.Empty, Disk.Empty, Disk.Empty, Disk.Empty)
+    val array6 = arrayOf(Disk.Red, Disk.Empty, Disk.Empty, Disk.Empty, Disk.Empty, Disk.Empty)
+    val array7 = arrayOf(Disk.Red, Disk.Empty, Disk.Empty, Disk.Empty, Disk.Empty, Disk.Empty)
+    val grid = arrayOf(array1, array2, array3, array4, array5, array6, array7)
 
     ConnectAppTheme {
-        DiskGrid(grid = grid, onColumnPressed = game::addDisk, emptyList())
+        Board(
+            grid = grid,
+            onColumnPressed = { },
+            winners = emptyList(),
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
